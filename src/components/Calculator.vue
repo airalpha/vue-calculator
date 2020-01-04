486510fb23b3f2eb46e87bb0bf28f6388897cd10
AIR ALPHA <allphaair@gmail.com>
2020-01-04 17:22:24 +0100

    End logic

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,32 +1,98 @@
 <template>
   <div class="calculator">
     <h1 class="title">Vue Calculator</h1>
-    <div class="display">0.067876</div>
-    <div class="btn">AC</div>
-    <div class="btn">+/-</div>
-    <div class="btn">%</div>
-    <div class="btn operator">/</div>
-    <div class="btn">7</div>
-    <div class="btn">8</div>
-    <div class="btn">9</div>
-    <div class="btn operator">x</div>
-    <div class="btn">4</div>
-    <div class="btn">5</div>
-    <div class="btn">6</div>
-    <div class="btn operator">-</div>
-    <div class="btn">1</div>
-    <div class="btn">2</div>
-    <div class="btn">3</div>
-    <div class="btn operator">+</div>
-    <div class="btn zero">0</div>
-    <div class="btn">.</div>
-    <div class="btn operator">=</div>
+    <div class="display">{{ current || 0 }}</div>
+    <div class="btn" @click="clear">AC</div>
+    <div class="btn" @click="sign">+/-</div>
+    <div class="btn" @click="percent">%</div>
+    <div class="btn operator" @click="divide">/</div>
+    <div class="btn" @click="write(7)">7</div>
+    <div class="btn" @click="write(8)">8</div>
+    <div class="btn" @click="write(9)">9</div>
+    <div class="btn operator" @click="times">x</div>
+    <div class="btn" @click="write(4)">4</div>
+    <div class="btn" @click="write(5)">5</div>
+    <div class="btn" @click="write(6)">6</div>
+    <div class="btn operator" @click="minus">-</div>
+    <div class="btn" @click="write(1)">1</div>
+    <div class="btn" @click="write(2)">2</div>
+    <div class="btn" @click="write(3)">3</div>
+    <div class="btn operator" @click="plus">+</div>
+    <div class="btn zero" @click="write(0)">0</div>
+    <div class="btn" @click="dot">.</div>
+    <div class="btn operator" @click="equal">=</div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      current: '',
+      operator: null,
+      previous: null,
+      operatorCliked: false
+    }
+  },
+  methods: {
+    clear() {
+      this.current = '';
+    },
 
+    sign() {
+      this.current = this.current * -1;
+    },
+
+    percent() {
+      this.current = `${parseFloat(this.current) / 100}`;
+    },
+
+    write(number) {
+      if(this.operatorCliked) {
+        this.current = '';
+        this.operatorCliked = false;
+      }
+      this.current += number.toString();
+    },
+
+    dot() {
+      //If dot not exist
+      if(this.current.indexOf('.') === -1) {
+        this.write('.');
+      }
+    },
+
+    setPrevious() {
+      this.previous = this.current;
+      this.operatorCliked = true;
+    },
+
+    divide() {
+      this.operator = (a, b) => a / b;
+      this.setPrevious()
+    },
+
+    times() {
+      this.operator = (a, b) => a * b;
+      this.setPrevious()
+    },
+
+    plus() {
+      this.operator = (a, b) => a + b;
+      this.setPrevious()
+    },
+
+    minus() {
+      this.operator = (a, b) => a - b;
+      this.setPrevious()
+    },
+
+    equal() {
+      this.current = `${this.operator(parseFloat(this.previous), parseFloat(this.current))}`;
+      this.previous = null;
+    }
+
+  }
 }
 </script>
 
